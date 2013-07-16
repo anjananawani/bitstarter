93cdc2ab6848e99be81c5a1b84b83d39bd2df7dd
Ubuntu <ubuntu@ip-172-31-23-65.us-west-2.compute.internal>
2013-07-16 05:13:50 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,7 +1,5 @@
-var express = require('express');
-
 var app = express.createServer(express.logger());
-
+var fs = require('fs');
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
 });
