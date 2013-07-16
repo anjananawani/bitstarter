0aead54659fa0068df9beaaa74ba254690fdda21
Ubuntu <ubuntu@ip-172-31-23-65.us-west-2.compute.internal>
2013-07-16 05:48:32 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ fs.readFile('./index.html', function (err, html) {
     if (err) {
         throw err; 
     }       
-    express.createServer(function(request, response) {  
+    app.get('/', function(request, response) {  
         response.writeHeader(200, {"Content-Type": "text/html"});  
         response.write(html);  
         response.end();  
